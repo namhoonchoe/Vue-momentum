dfc1a5435d2ffabf5ff8adf860630238cc7d1732
namhoonchoe <33464899+namhoonchoe@users.noreply.github.com>
2020-05-10 17:01:31 +0900

    bg Img
    bgImg #2

--- src/components/bgImg.vue
+++ src/components/bgImg.vue
@@ -1,13 +1,13 @@
 <template>
 <body>
     <img src="../assets/1.jpg" 
-         v-if="randNum === 1"
+         v-if="randNum == 1"
          alt="bgimg1">
     <img src="../assets/2.jpg" 
-         v-else-if="randNum === 2"
+         v-else-if="randNum == 2"
          alt="bgimg2">
     <img src="../assets/3.jpg" 
-         v-else-if="randNum === 3"
+         v-else-if="randNum == 3"
          alt="bgimg3">
     <img src="../assets/4.jpg" 
          v-else
@@ -18,23 +18,31 @@
 <script>
 export default {
     data(){
-        return{
-            randNum:''
-        }
+      return {
+        randNum: 0
+      }
     },
-    methods:{
-        genRandom() {
-            const imgNum = 4;
-            const randNum = Math.floor((Math.random)*imgNum);
-            return randNum;
-        }
-    },
-    mounted() {
-        this.randNum = this.genRandom()
+    methods :{
+      genRandom() {
+          const imgNum = 8;
+          this.randNum = Math.floor(Math.random()*imgNum);
+          return this.randNum
+      }     
+    } ,
+    created(){
+        this.genRandom();
     }
+
 }
+
 </script>
 
 <style>
-
+    .body{
+        position:absolute;
+        margin-top: 0px;
+        margin-left:0px;
+        width:100%;
+        z-index: -1;
+    }
 </style>
